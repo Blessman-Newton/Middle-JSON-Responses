d0b41e1c705abc3f68f76b15ac319ee7d54d564d
Blessman-Newton <blessmannewton0@gmail.com>
2024-09-16 23:39:02 +0000

    Add JsonResponses to view

--- Middle_JSON_Response/day_two/views.py
+++ Middle_JSON_Response/day_two/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def json_view(request):
+    data = {'message': 'Hello, JSON!'}
+    return JsonResponse(data)
